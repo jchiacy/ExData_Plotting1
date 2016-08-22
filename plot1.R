0fb9c17a7f07ab3da7e7c72595f25a8077db103c
Jasper Chia <jchiacy@users.noreply.github.com>
2016-08-22 21:00:38 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -6,7 +6,9 @@ unzip('./data/household_power_consumption.zip', exdir = './data')
 
 # read data into R
 files <- file('./data/household_power_consumption.txt')
-data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), sep = ';', col.names = c("Date", "Time", 
+    "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", 
+    "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
 
 # Plot 1
 # open device
